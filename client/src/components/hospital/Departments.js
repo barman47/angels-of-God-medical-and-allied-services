a05a2e5a03b6ab41a5e071acaa5143a15dfa763b
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2019-12-31 10:19:09 +0100

    Added Departments content

--- client/src/components/hospital/Departments.js
+++ client/src/components/hospital/Departments.js
@@ -4,54 +4,54 @@ import { Link } from 'react-router-dom';
 const Departments = () => (
     <section className="departments">
         <h4 className="header"><span className="underline">Dep</span>artments</h4>
-        <p className="header-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, vel fugit debitis provident corrupti autem. Ut quidem amet vero nam.</p>
+        <p className="header-text">Our hospital is multisectorial, consisting of various well-equipped departments to facilate optimum quality of care.</p>
         <section>
             <div className="department">
                 <div></div>
                 <h6>OBSTETRICS AND GYNECOLOGY</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>We provide exceptional care from pregnancy through childbirth, and postpartum periods.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>INTERNAL MEDICINE</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>Our highly skilled general medical practitioners provide unique services.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>SURGERY</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>Our surgeons deliver first class services for a wide spectrum of procedures.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>PAEDIATRICS</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>We zealously provide the ideal health care for children.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>PREVENTIVE MEDICINE</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>Prevention is always better cure, that's why we provide prophylactic care.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>LABORATORY AND RADIOLOGY</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>Our labs are well furnished with sophisticated machines for diverse procedures.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>MEDICAL PSYCHOLOGY</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>Excellent mental health care services are made available to our patients.</p>
                 <Link to="/">Learn More</Link>
             </div>
             <div className="department">
                 <div></div>
                 <h6>GERIATRICS</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <p>We provide diagnostic, treatment, preventive, and rehabilitative care for the elderly.</p>
                 <Link to="/">Learn More</Link>
             </div>
         </section>
