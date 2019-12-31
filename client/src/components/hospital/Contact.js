a3700ad0fce0d18ebd17764e71a75c1bb97b889d
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2019-12-31 02:46:22 +0100

    Added Departments

--- client/src/components/hospital/Contact.js
+++ client/src/components/hospital/Contact.js
@@ -5,8 +5,8 @@ const Contact = () => (
         <div className="contact-hospital">
             <div>
                 <div className="contact-info">
-                    <h5>For Any Emergency Contact</h5>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <h5>For Any Emergency</h5>
+                    <p>Contact us anytime, any day.</p>
                 </div>
                 <button>+234 7033501199</button>
             </div>
@@ -15,7 +15,7 @@ const Contact = () => (
             <div>
                 <div className="contact-info">
                     <h5>Make an Online Appointment</h5>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Schedule an appointment from your comfort zone</p>
                 </div>
                 <button>Make An Appointment</button>
             </div>
--- client/src/components/hospital/Departments.js
+++ client/src/components/hospital/Departments.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Departments = () => (
-    <h1>Departments</h1>
+    <section className="departments">
+        <h4 className="header"><span className="underline">Dep</span>artments</h4>
+        <p className="header-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, vel fugit debitis provident corrupti autem. Ut quidem amet vero nam.</p>
+        <section>
+            <div className="department">
+                <div></div>
+                <h6>OBSTETRICS AND GYNECOLOGY</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>INTERNAL MEDICINE</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>SURGERY</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>PAEDIATRICS</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>PREVENTIVE MEDICINE</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>LABORATORY AND RADIOLOGY</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>MEDICAL PSYCHOLOGY</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+            <div className="department">
+                <div></div>
+                <h6>GERIATRICS</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, accusantium.</p>
+                <Link to="/">Learn More</Link>
+            </div>
+        </section>
+    </section>
 );
 
 export default Departments;
