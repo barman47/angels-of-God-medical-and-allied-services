e4ab2fe33f6a3a95b60e264fd0e9538a8d4ca5e2
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2020-01-02 18:27:59 +0100

    Added Doctors

--- client/src/components/hospital/Doctors.js
+++ client/src/components/hospital/Doctors.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
 const Doctors = () => (
-    <h1>Doctors</h1>
+    <section className="doctors">
+        <h4>Expert Doctors</h4>
+        <div className="doctors-section">
+            <div className="card-container">
+                <div className="doctor">
+                    <div>
+                        <h5>Uzoanya Dominic</h5>
+                        <h6>Neorologist</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-container">
+
+                <div className="doctor">
+                    <div>
+                        <h5>Uzoanya Dominic</h5>
+                        <h6>Neorologist</h6>
+                    </div>
+                </div>    
+            </div>
+            
+            <div className="card-container">
+                <div className="doctor">
+                    <div>
+                        <h5>Uzoanya Dominic</h5>
+                        <h6>Neorologist</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-container">
+                <div className="doctor">
+                    <div>
+                        <h5>Uzoanya Dominic</h5>
+                        <h6>Neorologist</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 );
 
 export default Doctors;
